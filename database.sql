6b28e18e7390254c268bd4b832bbe7e70d6fa1d7
Lawleet <38157838+lawleet@users.noreply.github.com>
2018-07-01 16:23:40 +0545

    Database created, add all files

---
+++ database.sql
@@ -0,0 +1,13 @@
+create database test;
+
+use test;
+
+CREATE TABLE 'users' ( 
+ 'id' int(11) NOT NULL auto_increment, 
+ 'name' varchar(100) NOT NULL, 
+ 'age' int(3) NOT NULL, 
+ 'email' varchar(100) NOT NULL, 
+ 'password' varchar(100) NOT NULL, 
+ 'description' text NOT NULL, 
+PRIMARY KEY('id')
+ ); 
